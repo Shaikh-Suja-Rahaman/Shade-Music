5b96f8a7ae4dbbd6598a3d4427fa3088aa4b31a6
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2025-04-20 16:36:17 +0530

    albums demo 1

--- shade/src/components/AlbumItem.jsx
+++ shade/src/components/AlbumItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const AlbumItem = ({image, name, desc, id}) => {
+
+  const navigate = useNavigate()
+
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg=[#ffffff26]'>
+    <div onClick={()=>navigate(`/album/${id}`)} key={id} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26] transition-all ease-in-out duration-200'>
       <img src={image} className='rounded' alt=""/>
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
--- shade/src/components/DisplayHome.jsx
+++ shade/src/components/DisplayHome.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
 import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
@@ -21,7 +22,7 @@ const DisplayHome = () => {
         <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
         <div className='flex overflow-auto'>
         {songsData.map((item, index)=>(
-            <AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+            <SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
         ))}
         </div>
 
